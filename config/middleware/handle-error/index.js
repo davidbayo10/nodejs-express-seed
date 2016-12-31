4e8f0359c0244fc0c6a4fba1a8e6355fc2d699c5
David Bayo <dbayo@finametrix.com>
2016-12-31 13:55:37 +0100

    remove unnussed next function

--- config/middleware/handle-error/index.js
+++ config/middleware/handle-error/index.js
@@ -3,7 +3,7 @@ const logger = require('log4js').getLogger('Middleware:Handle-error');
 const messages = require('./error-messages');
 
 module.exports = function (app) {
-  app.use((err, req, res, next) => {
+  app.use((err, req, res) => {
     const type = err.type || 500;
     const message = messages[err.message] || err.message;
     logger.error(`[HTTP:${type}]`, req.method, req.url);
