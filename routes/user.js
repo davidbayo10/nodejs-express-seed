09671ae2253760f55e7a80b5faa01cc801bbcb6f
David Bayo <dbayo@finametrix.com>
2016-12-31 13:54:23 +0100

    routes out off project config

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 'use strict';
-const userController = require('../../src/user/user.controller');
+const userController = require('../src/user/user.controller');
 function userRouter(app) {
   const router = require('co-express-router')();
   router.get('/api/user/:id', userController.get);
