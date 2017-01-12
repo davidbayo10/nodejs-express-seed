3f9665d5b537562cbb2eb68e993f70bf4704d9a8
David Bayo <dbayo@finametrix.com>
2017-01-12 19:40:53 +0100

    corrected route for getting user by id

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@
 const userController = require('../src/user/user.controller');
 function userRouter(app) {
   const router = require('co-express-router')();
-  router.get('/api/user/:id', userController.get);
+  router.get('/api/users/:id', userController.get);
   router.post('/api/users', userController.create);
 
   app.use(router);
