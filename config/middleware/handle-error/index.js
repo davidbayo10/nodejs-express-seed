ff94af3546a2ee4adb3184ff1bf31985e190e296
David Bayo <dbayo@finametrix.com>
2017-01-15 15:38:13 +0100

    fix handle error middleware and messages

--- config/middleware/handle-error/index.js
+++ config/middleware/handle-error/index.js
@@ -3,13 +3,13 @@ const logger = require('log4js').getLogger('Middleware:Handle-error');
 const messages = require('./error-messages');
 
 module.exports = function (app) {
-  app.use((err, req, res) => {
+  app.use((err, req, res, next) => {
     const type = err.type || 500;
     const message = messages[err.message] || err.message;
     logger.error(`[HTTP:${type}]`, req.method, req.url);
     logger.error(req.headers['user-agent']);
     logger.error(message);
-    logger.error(err);
+    logger.error(err.stack || err);
     res.status(type).send(message);
   });
 };
